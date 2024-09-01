05b73efcc3e9181a6083fda1428c4af5fb6d7570
Sumit <paspolasumit2004@gmail.com>
2024-09-01 21:36:45 +0530

    oyes

--- src/Components/Video.js
+++ src/Components/Video.js
@@ -6,7 +6,7 @@ const Video = () => {
   return (
     <div className='videoContainer'>
 
-        <video src="https://iiokcprfxttdlszwhpma.supabase.co/storage/v1/object/public/video/km_20240901_1080p_30f_20240901_121136.mp4?t=2024-09-01T06%3A48%3A31.314Z" controls autoPlay></video>
+        <video src="https://iiokcprfxttdlszwhpma.supabase.co/storage/v1/object/public/video/km_20240901_1080p_30f_20240901_213003.mp4?t=2024-09-01T16%3A04%3A40.577Z" controls autoPlay></video>
 
         <p onClick={() => navigate('/registration')} className='skip'>Skip Video</p>
       
